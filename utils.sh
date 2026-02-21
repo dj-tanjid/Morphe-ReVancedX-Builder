58863ad2418a4b306237746a2d35a6a1ed10524a
Md. TanJidul Hossain <dj.tanjid.bracu@gmail.com>
2026-02-22 01:11:33 +0600

    Update utils.sh
    Adding custom lines and texts. Added banner feature which will show custom banner on Morphe Modules in supported Magisk & KSU.

--- utils.sh
+++ utils.sh
@@ -692,7 +692,8 @@ module_prop() {
 name=${2}
 version=v${3}
 versionCode=${NEXT_VER_CODE}
-author=j-hc
+author=dj_tanjid | j-hc
+banner=https://raw.githubusercontent.com/dj-tanjid/ReVancedX-Morphe-Builder/teejay/${1}/banner.png
 description=${4}" >"${6}/module.prop"
 
 	if [ "$ENABLE_MODULE_UPDATE" = true ]; then echo "updateJson=${5}" >>"${6}/module.prop"; fi
